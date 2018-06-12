1f3bca23b9f7f2453129f795516c74010cbabd2a
alexcorvi <alex@arrayy.com>
2018-06-12 20:33:13 +0300

    Removed icons

--- icons/svg-icon.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as icons from './svg-tsx/_index';
-
-export class SVGIcon extends React.Component<{ icon: keyof typeof icons }> {
-	render() {
-		return icons[this.props.icon];
-	}
-}
--- icons/svg-tsx/_index.ts
+++
@@ -1,5 +0,0 @@
-export { ampoul } from './ampoul';
-export { anesthesia } from './anesthesia-2';
-export { anesthesia2 } from './anesthesia';
-export { apple2 } from './apple';
-export { apple } from './apple-2';
