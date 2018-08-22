d280cb28225827fa04d571ac339fd030dfd90734
alexcorvi <alex@arrayy.com>
2018-08-22 13:49:17 +0300

    Use icon subset

--- src/app.tsx
+++ src/app.tsx
@@ -4,12 +4,15 @@ import 'antd/lib/grid/style/index.css';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { initializeIcons } from '@uifabric/icons';
 import { API, components } from './core';
-
 import { Fabric } from 'office-ui-fabric-react';
 
-initializeIcons();
+// generated from https://uifabricicons.azurewebsites.net/
+// 1. Download the subset
+// 2. put the font file in the dist/application/fonts/
+// 3. put the contents of src dir in './core/icons-subset/'
+import { initializeIcons } from './core/icons-subset';
+initializeIcons('./fonts/');
 
 ReactDOM.render(
 	<Fabric>
