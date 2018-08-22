6219eca5c200ce81cfe88c5e99873e9cd28f726f
alexcorvi <alex@arrayy.com>
2018-08-22 13:48:44 +0300

    Gallery not visible when offline

--- src/assets/components/gallery/gallery.tsx
+++ src/assets/components/gallery/gallery.tsx
@@ -1,7 +1,16 @@
 import './gallery.scss';
 import * as React from 'react';
 import { PickFile, fileTypes } from '../../../assets/components/pick-files/pick-files';
-import { Icon, Panel, PanelType, PrimaryButton, Spinner, SpinnerSize } from 'office-ui-fabric-react';
+import {
+	Icon,
+	Panel,
+	PanelType,
+	PrimaryButton,
+	Spinner,
+	SpinnerSize,
+	MessageBar,
+	MessageBarType
+} from 'office-ui-fabric-react';
 import { observable, observe } from 'mobx';
 import { observer } from 'mobx-react';
 import { API } from '../../../core/index';
@@ -26,83 +35,92 @@ export class Gallery extends React.Component<
 	render() {
 		return (
 			<div className="gallery-component">
-				<div className="gallery-label"> {this.props.label} </div>
-				{this.gallery.map(
-					(image) =>
-						this.galleryResolved[image] ? (
-							<div
-								onClick={() => (this.show = image)}
-								key={image}
-								className="image"
-								style={{ backgroundImage: `url(${this.galleryResolved[image]})` }}
-							/>
-						) : (
-							<div key={image} className="image" style={{ padding: '20px' }}>
-								<Spinner size={SpinnerSize.large} />
-							</div>
-						)
-				)}
 				{navigator.onLine ? (
 					<div>
-						<span
-							className="input-image"
-							onClick={() => {
-								if (this.pickFileInstance) {
-									this.loading = true;
-									this.pickFileInstance.click();
-								}
-							}}
+						<div className="gallery-label"> {this.props.label} </div>
+						{this.gallery.map(
+							(image) =>
+								this.galleryResolved[image] ? (
+									<div
+										onClick={() => (this.show = image)}
+										key={image}
+										className="image"
+										style={{ backgroundImage: `url(${this.galleryResolved[image]})` }}
+									/>
+								) : (
+									<div key={image} className="image" style={{ padding: '20px' }}>
+										<Spinner size={SpinnerSize.large} />
+									</div>
+								)
+						)}
+						<div>
+							<span
+								className="input-image"
+								onClick={() => {
+									if (this.pickFileInstance) {
+										this.loading = true;
+										this.pickFileInstance.click();
+									}
+								}}
+							>
+								<Icon
+									iconName={this.loading ? 'Sync' : 'Add'}
+									className={this.loading ? 'rotate' : ''}
+								/>
+								<span>Add Image</span>
+							</span>
+							<PickFile
+								accept={fileTypes.image}
+								onPick={(newFiles) => {
+									this.loading = false;
+									this.gallery.push(...newFiles);
+									this.props.onChange(toJS(this.gallery));
+								}}
+								multiple={this.props.single ? false : true}
+								ref={(instance) => (instance ? (this.pickFileInstance = instance) : '')}
+							/>
+						</div>
+						<Panel
+							isOpen={!!this.show}
+							type={PanelType.smallFluid}
+							onDismiss={() => (this.show = '')}
+							headerText=""
+							isFooterAtBottom={true}
+							onRenderFooterContent={() => (
+								<div>
+									<PrimaryButton
+										onClick={async () => {
+											this.loading = true;
+											this.gallery.splice(this.gallery.indexOf(this.show), 1);
+											this.props.onChange(toJS(this.gallery));
+											await API.files.remove(this.show);
+											this.show = '';
+											this.loading = false;
+										}}
+										className={`delete`}
+										iconProps={{ iconName: this.loading ? 'sync' : 'delete' }}
+										disabled={this.loading}
+									>
+										Delete
+									</PrimaryButton>
+								</div>
+							)}
 						>
-							<Icon iconName={this.loading ? 'Sync' : 'Add'} className={this.loading ? 'rotate' : ''} />
-							<span>Add Image</span>
-						</span>
-						<PickFile
-							accept={fileTypes.image}
-							onPick={(newFiles) => {
-								this.loading = false;
-								this.gallery.push(...newFiles);
-								this.props.onChange(toJS(this.gallery));
-							}}
-							multiple={this.props.single ? false : true}
-							ref={(instance) => (instance ? (this.pickFileInstance = instance) : '')}
-						/>
+							{this.galleryResolved[this.show] ? (
+								<img src={this.galleryResolved[this.show]} style={{ maxWidth: '100%' }} />
+							) : (
+								<Spinner size={SpinnerSize.large} />
+							)}
+						</Panel>
 					</div>
 				) : (
-					''
+					<div>
+						<br />
+						<MessageBar messageBarType={MessageBarType.warning}>
+							{"Attachments are not available while you're offline."}
+						</MessageBar>
+					</div>
 				)}
-
-				<Panel
-					isOpen={!!this.show}
-					type={PanelType.smallFluid}
-					onDismiss={() => (this.show = '')}
-					headerText=""
-					isFooterAtBottom={true}
-					onRenderFooterContent={() => (
-						<div>
-							<PrimaryButton
-								onClick={async () => {
-									this.loading = true;
-									this.gallery.splice(this.gallery.indexOf(this.show), 1);
-									this.props.onChange(toJS(this.gallery));
-									await API.files.remove(this.show);
-									this.show = '';
-									this.loading = false;
-								}}
-								className={`delete`}
-								iconProps={{ iconName: this.loading ? 'sync' : 'trash' }}
-								disabled={this.loading}
-							>
-								Delete
-							</PrimaryButton>
-						</div>
-					)}
-				>
-					{this.galleryResolved[this.show] ? (
-						<img src={this.galleryResolved[this.show]} style={{ maxWidth: '100%' }} />
-					) : (
-						<Spinner size={SpinnerSize.large} />
-					)}
-				</Panel>
 			</div>
 		);
 	}
