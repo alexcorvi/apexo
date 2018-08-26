0a279958cd6fc0447fd5303e62e29f92cf74fdaa
alexcorvi <alex@arrayy.com>
2018-08-26 12:05:27 +0300

    added desktop build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,2 @@
+nativefier --name "apexo" --platform osx --conceal --icon icon-final.png --min-width 450px --min-height 600px --disable-dev-tools "https://web.apexo.app" ./dist/desktop-builds/
+nativefier --name "apexo" --platform windows --icon icon-final.ico --min-width 450px --min-height 600px --disable-dev-tools "https://web.apexo.app" ./dist/desktop-builds/
