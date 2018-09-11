c4a0cc706738cf96e299951909d428ae293582f3
alexcorvi <alex@arrayy.com>
2018-09-11 13:55:51 +0300

    better experience when choosing doctor

--- src/core/login/choose-doctor.tsx
+++ src/core/login/choose-doctor.tsx
@@ -1,77 +1,54 @@
-import './login.scss';
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { API } from '../';
+import { data } from '../../modules';
 import { Dropdown, PrimaryButton, TextField } from 'office-ui-fabric-react';
 import { Label, LabelType } from '../../assets/components/label/label.component';
-import { data } from '../../modules';
 import { login, LoginStep } from './data.login';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
+import { Profile } from '../../assets/components/profile/profile';
+import './login.scss';
 
 @observer
 export class ChooseDoctor extends React.Component<{}, {}> {
-	@observable newDoc: boolean = false;
-	newDocNameTextField: TextField | undefined;
+	@observable newDocName: string = '';
 	render() {
 		return (
 			<div className="login-component">
-				<div className="pivot m-b-10">
-					{data.doctorsData.doctors.list.length > 0 ? (
-						<div>
-							<Label
-								text="Already registered"
-								type={!this.newDoc ? LabelType.primary : LabelType.info}
-								onClick={() => (this.newDoc = false)}
-							/>
-							<Label
-								text="New Doctor"
-								type={this.newDoc ? LabelType.primary : LabelType.info}
-								onClick={() => (this.newDoc = true)}
+				<div className="m-b-5">
+					{data.doctorsData.doctors.list.map((doctor) => (
+						<div
+							className="m-t-5 p-5 doctor-choose pointer"
+							onClick={() => {
+								login.setDoctor(doctor._id);
+							}}
+							key={doctor._id}
+						>
+							<Profile
+								size={3}
+								name={doctor.name}
+								secondaryElement={<span>{doctor.sortedDays.join(', ')}</span>}
 							/>
 						</div>
-					) : (
-						''
-					)}
+					))}
 				</div>
-				{this.newDoc || data.doctorsData.doctors.list.length === 0 ? (
-					<div>
-						<TextField
-							ref={(field) => (field ? (this.newDocNameTextField = field) : '')}
-							label="Your name"
-						/>
-						<PrimaryButton
-							text="Register"
-							onClick={() => {
-								if (!this.newDocNameTextField) {
-									return;
-								}
-								if (!this.newDocNameTextField.value) {
-									return;
-								}
-								const newDoctor = new data.doctorsData.Doctor();
-								newDoctor.name = this.newDocNameTextField.value;
-								data.doctorsData.doctors.list.push(newDoctor);
-								login.currentDoctorID = newDoctor._id;
-							}}
-						/>
-					</div>
-				) : (
-					<Dropdown
-						placeHolder="Who are you?"
-						options={data.doctorsData.doctors.list.map((doctor) => {
-							return {
-								key: doctor._id,
-								text: doctor.name
-							};
-						})}
-						onChanged={(doctor) => {
-							login.setDoctor(doctor.key.toString());
-							API.router.go([ '' ]);
-						}}
-					/>
-				)}
+				<hr />
+				<TextField
+					value={this.newDocName}
+					onChanged={(v) => (this.newDocName = v)}
+					label={'Register new doctor'}
+				/>
+				<PrimaryButton
+					onClick={() => {
+						const newDoc = new data.doctorsData.Doctor();
+						newDoc.name = this.newDocName;
+						this.newDocName = '';
+						data.doctorsData.doctors.list.push(newDoc);
+					}}
+				>
+					Register
+				</PrimaryButton>
 			</div>
 		);
 	}
