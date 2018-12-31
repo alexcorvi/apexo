68d8f04a7f30d669866f7e1792810967f1092711
alexcorvi <alex@arrayy.com>
2019-01-01 01:55:58 +0300

    Remove date filters from appointments lists fixes #25

--- src/assets/components/appointments-list/appointments-list.tsx
+++ src/assets/components/appointments-list/appointments-list.tsx
@@ -19,26 +19,13 @@ import { textualFilter } from '../../../assets/utils/textual-filter';
 
 @observer
 export class AppointmentsList extends React.Component<{ list: Appointment[] }, {}> {
-	@observable from: number = new Date().getTime() - 86400000 * 7;
-	@observable to: number = new Date().getTime() + 86400000 * 7;
 	@observable filter: string = '';
 
 	@observable selectedAppointmentID: string = '';
 
 	@computed
-	get dateFiltered() {
-		return this.props.list.filter((appointment) => {
-			return appointment.date > this.from && appointment.date < this.to;
-		});
-	}
-
-	@computed
 	get filtered() {
-		return this.filter ? textualFilter(this.dateFiltered, this.filter) : this.dateFiltered;
-	}
-
-	openAppointment(id: string) {
-		this.selectedAppointmentID = id;
+		return this.filter ? textualFilter(this.props.list, this.filter) : this.props.list;
 	}
 
 	render() {
@@ -46,33 +33,8 @@ export class AppointmentsList extends React.Component<{ list: Appointment[] }, {
 			<div className="appointments-list">
 				{this.props.list.length > 0 ? (
 					<div className="main">
-						<Row gutter={6}>
-							<Col md={8}>
-								<DatePicker
-									label="From"
-									value={new Date(this.from)}
-									onSelectDate={(d) => {
-										if (d) {
-											this.from = d.getTime();
-										}
-									}}
-								/>
-							</Col>
-							<Col md={8}>
-								<DatePicker
-									label="To"
-									value={new Date(this.to)}
-									onSelectDate={(d) => {
-										if (d) {
-											this.to = d.getTime();
-										}
-									}}
-								/>
-							</Col>
-							<Col md={8}>
-								<TextField label="Filter" value={this.filter} onChanged={(v) => (this.filter = v)} />
-							</Col>
-						</Row>
+						<TextField label="Filter" value={this.filter} onChanged={(v) => (this.filter = v)} />
+
 						<hr />
 						<p
 							style={{
