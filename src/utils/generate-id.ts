d9c6ae0b16da1d8712a2b47774e6fb6d9c77ee6b
Alex Corvi <ali.a.saleem@outlook.com>
2019-04-12 17:58:52 +0300

    generating smaller IDs

--- src/utils/generate-id.ts
+++ src/utils/generate-id.ts
@@ -1,11 +1,11 @@
-export function generateID(len: number = 20, constant?: string) {
+export function generateID(len: number = 10, constant?: string) {
 	function dec2hex(dec: number) {
-		return ('0' + dec.toString(16)).substr(-2);
+		return ("0" + dec.toString(16)).substr(-2);
 	}
 	if (!constant) {
-		const arr = new Uint8Array((len || 20) / 2);
+		const arr = new Uint8Array((len || 10) / 2);
 		window.crypto.getRandomValues(arr);
-		return Array.from(arr, dec2hex).join('');
+		return Array.from(arr, dec2hex).join("");
 	} else {
 		return constant;
 	}
