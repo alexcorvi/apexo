b2d8b80dd92dbf469206b090593a5aaeed985fc7
Alex Corvi <ali.a.saleem@outlook.com>
2019-04-12 17:59:34 +0300

    Update textual-filter.ts

--- src/utils/textual-filter.ts
+++ src/utils/textual-filter.ts
@@ -1,10 +1,17 @@
-import { escapeRegExp } from './escape-regex';
-export function textualFilter<T extends { searchableString: string }>(list: T[], text: string) {
-	return list.filter((item) => {
-		const filters = text.split(' ').map((filterString) => new RegExp(escapeRegExp(filterString), 'gim'));
+import { escapeRegExp } from "@utils";
+export function textualFilter<T extends { searchableString: string }>(
+	list: T[],
+	text: string
+) {
+	return list.filter(item => {
+		const filters = text
+			.split(" ")
+			.map(filterString => new RegExp(escapeRegExp(filterString), "gim"));
 		if (!text) {
 			return true;
 		}
-		return filters.every((filter) => filter.test(item.searchableString.toString()));
+		return filters.every(filter =>
+			filter.test(item.searchableString.toString())
+		);
 	});
 }
