83429451c2a7f6aec3cb048aad1229cc15920675
Alex Corvi <ali.a.saleem@outlook.com>
2019-04-20 17:45:54 +0300

    more comments

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -3,9 +3,9 @@ import { decrypt, encrypt } from "@utils";
 export const dict = {
 	LSL_hash: "", // when truly logging in
 	LSL_TS: "", // when starting
-	user_id: "",
-	no_server_mode: "",
-	server_location: ""
+	user_id: "", // current user ID
+	no_server_mode: "", // is in no-server-mode
+	server_location: "" // current server location
 };
 
 export const store = {
