149ef508d75fc429bd625ea7d2159d00e044fb54
Alex Corvi <ali.a.saleem@outlook.com>
2019-04-21 01:25:31 +0300

    #89 testing user

---
+++ src/core/user.test.ts
@@ -0,0 +1,57 @@
+import "../mocks/browser-mocks";
+import "../mocks/state-mocks";
+import { user } from "@core";
+import * as core from "@core";
+import * as modules from "@modules";
+import { toJS } from "mobx";
+
+function setUserA() {
+	core.status.setUser(modules.staff.list.find(x => x.name === "A")!._id);
+}
+
+function setUserB() {
+	core.status.setUser(modules.staff.list.find(x => x.name === "B")!._id);
+}
+
+describe("@core: user panel", () => {
+	it("User panel is hidden by default", () => {
+		user.hide();
+		expect(user.isVisible).toBe(false);
+	});
+
+	it("Showing the user panel", () => {
+		user.show();
+		expect(user.isVisible).toBe(true);
+	});
+
+	it("Hiding the user panel", () => {
+		user.hide();
+		expect(user.isVisible).toBe(false);
+	});
+});
+
+describe("@core: user A info", function(this: any) {
+	it("Staff name", () => {
+		setUserA();
+		expect(user.currentUser!.name).toBe("A");
+	});
+
+	it("Today appointments", () => {
+		setUserA();
+		expect(user.todayAppointments.length).toBe(1);
+		expect(user.todayAppointments[0].notes).toBe("A");
+	});
+});
+
+describe("@core: user B info", () => {
+	it("Staff name", () => {
+		setUserB();
+		expect(user.currentUser!.name).toBe("B");
+	});
+
+	it("Today appointments", () => {
+		setUserB();
+		expect(user.todayAppointments.length).toBe(1);
+		expect(user.todayAppointments[0].notes).toBe("B");
+	});
+});
