5adffd05f46c0ed876b1057b40e8189b84a48082
Alex Corvi <ali.a.saleem@outlook.com>
2019-05-03 23:59:39 +0300

    #91 testing choose user

--- src/main-components/choose-user.tsx
+++ src/main-components/choose-user.tsx
@@ -21,50 +21,16 @@ export class ChooseUserComponent extends React.Component<
 	render() {
 		return (
 			<div className="login-component">
-				<div className="m-b-5">
-					{this.props.users.map(user => (
-						<div
-							className="m-t-5 p-5 user-chooser pointer"
-							onClick={() => {
-								if (user.pin) {
-									this.props.showModal({
-										id: generateID(),
-										text: text("Please enter your PIN"),
-										onConfirm: providedPin => {
-											if (providedPin === user.pin) {
-												this.props.onClickUser(
-													user._id
-												);
-											} else {
-												this.props.showMessage({
-													id: generateID(),
-													text: text(
-														`Invalid PIN provided`
-													)
-												});
-											}
-										},
-										input: true,
-										showCancelButton: true,
-										showConfirmButton: true
-									});
-								} else {
-									this.props.onClickUser(user._id);
-								}
-							}}
-							key={user._id}
-						/>
-					))}
-				</div>
-				<hr />
 				{this.props.users.length === 0 ? (
-					<div>
+					<div id="create-user">
 						<TextField
+							id="new-user-name"
 							value={this.newDocName}
 							onChange={(ev, v) => (this.newDocName = v!)}
 							label={text("Register as new staff member")}
 						/>
 						<PrimaryButton
+							id="create-new-user-btn"
 							onClick={() => {
 								this.props.onCreatingNew(this.newDocName);
 							}}
@@ -72,7 +38,42 @@ export class ChooseUserComponent extends React.Component<
 						/>
 					</div>
 				) : (
-					""
+					<div id="choose-user">
+						{this.props.users.map(user => (
+							<div
+								className="m-t-5 p-5 user-chooser pointer"
+								onClick={() => {
+									if (user.pin) {
+										this.props.showModal({
+											id: generateID(),
+											text: text("Please enter your PIN"),
+											onConfirm: providedPin => {
+												if (providedPin === user.pin) {
+													this.props.onClickUser(
+														user._id
+													);
+												} else {
+													this.props.showMessage({
+														id: generateID(),
+														text: text(
+															`Invalid PIN provided`
+														)
+													});
+												}
+											},
+											input: true,
+											showCancelButton: true,
+											showConfirmButton: true
+										});
+									} else {
+										this.props.onClickUser(user._id);
+									}
+								}}
+								key={user._id}
+								id={user._id}
+							/>
+						))}
+					</div>
 				)}
 			</div>
 		);
