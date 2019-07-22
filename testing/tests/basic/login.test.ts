8badf6a325ebe0692c1c07764ca04da3913b47bb
Alex Corvi <ali.a.saleem@outlook.com>
2019-07-22 18:52:17 +0300

    loginWithPIN test

--- testing/tests/basic/login.test.ts
+++ testing/tests/basic/login.test.ts
@@ -36,5 +36,45 @@ export const loginSuite: TestSuite = {
 		app.reset();
 		await interact.waitForEl(".input-server input");
 		assert.elValue(".input-server input", "http://localhost:5984");
+	},
+
+	async loginWithPIN() {
+		// start no server
+		await interact.waitAndClick(".no-server-mode");
+		await interact.waitAndClick(".ms-Persona");
+		// go to staff page and input a PIN
+		await interact.waitForEl(".main-component");
+		await app.goToPage("staff");
+		await interact.waitForEl(`.staff-component`);
+		await interact.waitAndClick(".alexcorvi .permission");
+		await interact.waitForEl("#login-pin");
+		await interact.typeIn("#login-pin", "1234");
+		// visit a couple of routes
+		await app.goToPage("patients");
+		await interact.waitForEl(`.patients-component`);
+		await app.goToPage("staff");
+		await interact.waitForEl(`.staff-component`);
+		// and then back to PIN to make sure its there
+		await interact.waitAndClick(".alexcorvi .permission");
+		assert.elValue("#login-pin", "1234");
+		// reset and go to no server mode again
+		await app.reset();
+		await interact.waitAndClick(".no-server-mode");
+		await interact.waitForEl("#choose-user");
+		// now should be asked for a PIN
+		// we'll enter an invalid one first
+		await interact.waitAndClick(".ms-Persona");
+		await interact.waitForEl("#modal-input");
+		await interact.typeIn("#modal-input", "12345"); // invalid
+		interact.waitAndClick("#confirm-modal-btn");
+		// expect an error message
+		await interact.waitForEl(".message-inner");
+		// we'll enter the valid one now
+		interact.waitAndClick(".ms-Persona");
+		await interact.waitForEl("#modal-input");
+		await interact.typeIn("#modal-input", "1234"); // valid
+		interact.waitAndClick("#confirm-modal-btn");
+		// we should be in
+		await interact.waitForEl(".header-component");
 	}
 };
