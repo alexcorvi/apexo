03c0af126b5cb89cc1c86fca75a9a6d9550a78b5
Alex Corvi <ali.a.saleem@outlook.com>
2019-07-22 18:51:51 +0300

    fixtures?

--- testing/tests/basic/login.test.ts
+++ testing/tests/basic/login.test.ts
@@ -7,26 +7,29 @@ import { TestResult, TestSuite } from "../interface";
  * TODO: login while offline using saved session
  * TODO: login for user with PIN
  */
+
+const staffName = "Alex Corvi";
+
 export const loginSuite: TestSuite = {
 	async noServer(): TestResult {
 		// start no server
 		await app.reset();
 		await interact.waitAndClick(".no-server-mode");
 		// create new staff member
-		await interact.waitAndInput("#new-user-name", "staff A");
+		await interact.waitAndInput("#new-user-name", staffName);
 		interact.waitAndClick("#create-new-user-btn");
 
 		// go to staff page and make sure it's there
 		await interact.waitForEl(".main-component");
 		await app.goToPage("staff");
 		await interact.waitForEl(`.staff-component`);
-		assert.elContains(".staff-component", "staff A");
+		assert.elContains(".staff-component", staffName);
 
 		// after resetting the application, staff member must still be there
 		await app.reset();
 		await interact.waitAndClick(".no-server-mode");
 		await interact.waitForEl("#choose-user");
-		assert.elContains("#choose-user", "staff A");
+		assert.elContains("#choose-user", staffName);
 	},
 
 	async defaultServerField() {
