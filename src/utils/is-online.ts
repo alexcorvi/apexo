63a77257ca2e0fb356e73de3fd6e9c01a8c13731
Alex Corvi <ali.a.saleem@outlook.com>
2019-07-25 09:21:11 +0300

    use is-online instead of navigator.onLine

--- src/utils/is-online.ts
+++ src/utils/is-online.ts
@@ -1,7 +1,18 @@
 import { checkServer } from "@utils";
+import isClientOnlineLib from "is-online";
+export const connSetting = {
+	emulateOffline: false
+};
 export async function isOnline(server: string) {
-	if (!navigator.onLine) {
+	if (!(await isClientOnline())) {
 		return false;
 	}
 	return await checkServer(server);
+}
+
+export async function isClientOnline() {
+	if (connSetting.emulateOffline) {
+		return false;
+	}
+	return await isClientOnlineLib();
 }
