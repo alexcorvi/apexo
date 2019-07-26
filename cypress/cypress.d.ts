ce2ba2c0a9a267d1ef729e222f477ff54c4d4c47
Alex Corvi <ali.a.saleem@outlook.com>
2019-07-26 20:37:55 +0300

    added additional cypress commands with TS and write commands in TS

--- cypress/cypress.d.ts
+++ cypress/cypress.d.ts
@@ -1,2 +1,15 @@
 declare namespace Cypress {
+	interface Chainable {
+		offline(): void;
+		online(): void;
+		resetEverything(): void;
+		getByTestId(
+			testid: string,
+			timeout?: number
+		): Chainable<JQuery<HTMLElement>>;
+		ensureLoginType(type: string): void;
+		ensurePage(namespace: string): void;
+		goToPage(namespace: string): void;
+		slowType(input: string): void;
+	}
 }
---
+++ cypress/support/commands.ts
@@ -0,0 +1,82 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("offline", () => {
+	cy.window().then(win => (win as any).emulateOffline());
+});
+
+Cypress.Commands.add("online", () => {
+	cy.window().then(win => (win as any).disableOfflineEmulation());
+});
+
+Cypress.Commands.add("getByTestId", (testid, timeout = 4000) => {
+	return cy.get(`[data-testid="${testid}"]`, { timeout });
+});
+
+Cypress.Commands.add("ensureLoginType", type => {
+	cy.get(".header-component", { timeout: 5000 }).should(
+		"have.attr",
+		"data-login-type",
+		type
+	);
+});
+
+Cypress.Commands.add("resetEverything", type => {
+	cy.window({ timeout: 10 * 1000 })
+		.then(win => (win as any).hardResetApp())
+		.then(x => {
+			cy.clearCookies();
+			cy.clearLocalStorage();
+			(window as any).indexedDB.databases().then(r => {
+				for (let i = 0; i < r.length; i++) {
+					window.indexedDB.deleteDatabase(r[i].name);
+				}
+			});
+		});
+});
+
+Cypress.Commands.add("goToPage", namespace => {
+	console.log(namespace);
+	cy.getByTestId("expand-menu").click();
+	cy.get(`[title="${namespace.toLowerCase()}"].ms-Nav-link`).click();
+	cy.ensurePage(namespace);
+});
+
+Cypress.Commands.add("ensurePage", namespace => {
+	cy.get(
+		`[data-current-namespace="${namespace.toLowerCase()}"]#router-outlet`
+	);
+});
+
+Cypress.Commands.add(
+	"slowType",
+	{ prevSubject: "element" },
+	(subject, input) => {
+		input.split("").forEach(x => {
+			cy.get(subject[0]).type(x);
+			cy.wait(700);
+		});
+	}
+);
