6e7432957e0c73e43de9ffb2b957992ab0b3e1d3
Alex Corvi <ali.a.saleem@outlook.com>
2019-07-26 20:32:36 +0300

    cypress: support typescript

---
+++ cypress/plugins/index.js
@@ -0,0 +1,28 @@
+const wp = require("@cypress/webpack-preprocessor");
+
+module.exports = on => {
+	const options = {
+		webpackOptions: {
+			resolve: {
+				extensions: [".ts", ".js"]
+			},
+			module: {
+				rules: [
+					{
+						test: /\.ts$/,
+						exclude: [/node_modules/],
+						use: [
+							{
+								loader: "ts-loader",
+								options: {
+									transpileOnly: true
+								}
+							}
+						]
+					}
+				]
+			}
+		}
+	};
+	on("file:preprocessor", wp(options));
+};
