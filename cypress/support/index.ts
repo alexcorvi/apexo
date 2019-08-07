fc86e771f9011fcd8bdb3f8c1872562e339edf32
Alex Corvi <ali.a.saleem@outlook.com>
2019-08-07 11:49:04 +0300

    hide xhr requests

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -18,3 +18,11 @@ import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.Server.defaults({
+	delay: 0,
+	force404: false,
+	whitelist: xhr => {
+		return true;
+	}
+});
