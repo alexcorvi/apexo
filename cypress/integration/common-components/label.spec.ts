08de9ede24dfb79063f4a1f79a048aa1d6a86ad3
Alex Corvi <ali.a.saleem@outlook.com>
2019-09-06 15:09:37 +0300

    Update label.spec.ts

--- cypress/integration/common-components/label.spec.ts
+++ cypress/integration/common-components/label.spec.ts
@@ -2,7 +2,7 @@
 /// <reference types="../../cypress" />
 
 describe("label", () => {
-	beforeEach(() => {
+	before(() => {
 		cy.visit("http://localhost:8000");
 		cy.clearCookies();
 		cy.resetEverything();
@@ -17,26 +17,41 @@ describe("label", () => {
 	it("Viewing labels", () => {
 		const patients = ["alex", "dina", "william", "jeff", "bill", "hugh"];
 		cy.goToPage("patients");
-		patients.forEach(patientName => {
+		patients.forEach((patientName, index) => {
 			cy.get(`[title="Add new"]`).click();
 			cy.getByTestId("patient-name").type(patientName);
-			cy.closePanel();
-		});
-
-		patients.forEach(patientName => {
-			cy.get(".pg-pn-" + patientName + " .clickable .ms-Persona").click();
 			cy.get(".patient-labels input").type("label-" + patientName);
 			cy.get(".ms-Suggestions-item")
 				.first()
 				.click();
+			if (index % 2 === 0) {
+				cy.get(".patient-labels input").type("even");
+				cy.get(".ms-Suggestions-item")
+					.first()
+					.click();
+			}
 			cy.closePanel();
 		});
-
 		patients.forEach(patientName => {
 			cy.get(".pg-pn-" + patientName).should(
 				"contain.text",
 				"label-" + patientName
 			);
 		});
+	});
+	it("clicking labels", () => {
+		cy.get('[data-head="Patient"]').should("have.length.gt", 3);
+		cy.get(".label.clickable")
+			.contains("even")
+			.first()
+			.click();
+		cy.get(".label.clickable")
+			.contains("even")
+			.should("have.class", "highlighted");
+		cy.get('[data-head="Patient"]').should("have.length", 3);
+		cy.get(".label.clickable.highlighted")
+			.first()
+			.click();
+		cy.get('[data-head="Patient"]').should("have.length.gt", 3);
 	});
 });
