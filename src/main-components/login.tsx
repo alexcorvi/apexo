b4f7f268d5d6046fbda3a5aa5e713b1421da1e54
Alex Corvi <ali.a.saleem@outlook.com>
2020-06-01 19:44:51 +0300

    new login screen

--- src/main-components/login.tsx
+++ src/main-components/login.tsx
@@ -3,6 +3,7 @@ import * as core from "@core";
 import { second, store } from "@utils";
 import { computed, observable } from "mobx";
 import { observer } from "mobx-react";
+import { Link, PivotItem } from "office-ui-fabric-react";
 import * as React from "react";
 import {
 	DefaultButton,
@@ -12,6 +13,8 @@ import {
 	Spinner,
 	SpinnerSize,
 	TextField,
+	Pivot,
+	Icon,
 } from "office-ui-fabric-react";
 
 @observer
@@ -27,6 +30,8 @@ export class LoginView extends React.Component {
 	@observable disableInputs: boolean = false;
 
 	@observable initiallyChecked: boolean = false;
+
+	@observable formType: "s" | "c" = "s";
 
 	@computed get impossibleToLogin() {
 		return (
@@ -76,65 +81,30 @@ export class LoginView extends React.Component {
 		}
 	}
 
+	async loginSupported() {}
+
 	render() {
 		return (
 			<div className="login-component">
-				{this.impossibleToLogin ? (
-					<div data-testid="impossible">
-						<MessageBar messageBarType={MessageBarType.error}>
-							You're offline and unable to login
-							<br />
-						</MessageBar>
-						<DefaultButton
-							text="Reload"
-							className="m-t-15 m-b-15 m-r-5"
-							onClick={() => {
-								location.reload();
-							}}
-							iconProps={{
-								iconName: "Sync",
-							}}
-						/>
-					</div>
-				) : (
-					<div className="login-forum">
-						{this.initiallyChecked ? (
-							<div className="login-step">
-								<div
-									className={
-										status.isOnline.server ||
-										!store.found("LSL_hash")
-											? "hidden"
-											: "offline-msg"
-									}
-								>
-									<MessageBar
-										messageBarType={MessageBarType.warning}
-									>
-										{`${text(
-											`you're offline. use the latest username/password you've successfully used on this machine to login to this server`
-										)}:
-								${(this.serverFieldValue || "").replace(/([^\/])\/[^\/].+/, "$1")}.
-							`}
-									</MessageBar>
-								</div>
-
-								<div>
-									<TextField
-										name="server"
-										label={text(`server location`).c}
-										value={this.serverFieldValue}
-										disabled={this.disableInputs}
-										onChange={(ev, v) =>
-											(this.serverFieldValue = v!)
-										}
-										className="input-server"
-									/>
-								</div>
-
+				{this.initiallyChecked ? (
+					<Pivot
+						selectedKey={this.formType}
+						onLinkClick={(item) => {
+							if (item && item.props.itemKey) {
+								this.formType = item.props.itemKey as "c" | "s";
+							}
+							this.errorMessage = "";
+						}}
+					>
+						<PivotItem
+							headerText={"Supported"}
+							itemIcon={"Medical"}
+							itemKey={"s"}
+							key={"s"}
+						>
+							<div className="supported-login-forum">
 								<TextField
-									name="identification"
-									label={text(`username`).c}
+									label="Clinic ID"
 									disabled={this.disableInputs}
 									value={this.usernameFieldValue}
 									onChange={(e, v) =>
@@ -143,7 +113,7 @@ export class LoginView extends React.Component {
 									data-testid="input-identification"
 									onKeyDown={(ev) => {
 										if (ev.keyCode === 13) {
-											this.login();
+											this.loginSupported();
 										}
 									}}
 								/>
@@ -159,7 +129,7 @@ export class LoginView extends React.Component {
 									data-testid="input-password"
 									onKeyDown={(ev) => {
 										if (ev.keyCode === 13) {
-											this.login();
+											this.loginSupported();
 										}
 									}}
 								/>
@@ -172,55 +142,163 @@ export class LoginView extends React.Component {
 									className="m-t-15 m-b-15"
 									data-testid="proceed-primary"
 									onClick={() => {
-										this.login();
+										this.loginSupported();
 									}}
 								/>
-								{core.status.tryOffline ? (
-									<PrimaryButton
-										text={text("access offline").c}
+							</div>
+						</PivotItem>
+						<PivotItem
+							headerText={"Community"}
+							itemIcon={"Database"}
+							itemKey={"c"}
+							key={"c"}
+						>
+							<div className="community-login-forum">
+								<div className="login-step">
+									<div
+										className={
+											status.isOnline.server ||
+											!store.found("LSL_hash")
+												? "hidden"
+												: "offline-msg"
+										}
+									>
+										<MessageBar
+											messageBarType={
+												MessageBarType.warning
+											}
+										>
+											{`${text(
+												`you're offline. use the latest username/password you've successfully used on this machine to login to this server`
+											)}:
+								${(this.serverFieldValue || "").replace(/([^\/])\/[^\/].+/, "$1")}.
+							`}
+										</MessageBar>
+									</div>
+
+									<div>
+										<TextField
+											name="server"
+											label={text(`server location`).c}
+											value={this.serverFieldValue}
+											disabled={this.disableInputs}
+											onChange={(ev, v) =>
+												(this.serverFieldValue = v!)
+											}
+											className="input-server"
+										/>
+									</div>
+
+									<TextField
+										name="identification"
+										label={text(`username`).c}
 										disabled={this.disableInputs}
-										className="m-t-15 m-b-15 m-l-5 m-r-5"
-										data-testid="proceed-offline"
-										onClick={() => {
-											this.login(true);
+										value={this.usernameFieldValue}
+										onChange={(e, v) =>
+											(this.usernameFieldValue = v!)
+										}
+										data-testid="input-identification"
+										onKeyDown={(ev) => {
+											if (ev.keyCode === 13) {
+												this.login();
+											}
 										}}
 									/>
-								) : (
-									""
-								)}
-								<DefaultButton
-									onClick={() => core.status.startNoServer()}
-									className="m-t-15 m-b-15 m-l-5 m-r-5"
-									iconProps={{
-										iconName: "StatusErrorFull",
-									}}
+									<TextField
+										name="password"
+										type="Password"
+										label={text(`password`).c}
+										disabled={this.disableInputs}
+										value={this.passwordFieldValue}
+										onChange={(e, v) =>
+											(this.passwordFieldValue = v!)
+										}
+										data-testid="input-password"
+										onKeyDown={(ev) => {
+											if (ev.keyCode === 13) {
+												this.login();
+											}
+										}}
+									/>
+									<PrimaryButton
+										iconProps={{
+											iconName: "Permissions",
+										}}
+										text={text("login").c}
+										disabled={this.disableInputs}
+										className="m-t-15 m-b-15"
+										data-testid="proceed-primary"
+										onClick={() => {
+											this.login();
+										}}
+									/>
+									{core.status.tryOffline ? (
+										<PrimaryButton
+											text={text("access offline").c}
+											disabled={this.disableInputs}
+											className="m-t-15 m-b-15 m-l-5 m-r-5"
+											data-testid="proceed-offline"
+											onClick={() => {
+												this.login(true);
+											}}
+										/>
+									) : (
+										""
+									)}
+								</div>
+							</div>
+						</PivotItem>
+						<PivotItem
+							headerText={"Offline"}
+							itemIcon={"WifiWarning4"}
+							itemKey={"o"}
+							key={"o"}
+						>
+							<div className="offline-login-form">
+								<MessageBar
+									messageBarType={MessageBarType.warning}
 								>
-									no-server mode
-								</DefaultButton>
+									When using the application in offline mode
+									all data is susceptible to loss, as it would
+									only live in your browser/computer. It won't
+									sync with other devices, and many features
+									of the application will be disabled.
+								</MessageBar>
+								<div>
+									<PrimaryButton
+										iconProps={{ iconName: "WifiWarning4" }}
+										onClick={() => {
+											core.status.startNoServer();
+										}}
+									>
+										Use offline
+									</PrimaryButton>
+								</div>
 							</div>
-						) : (
-							<div className="spinner-container">
-								<Spinner
-									size={SpinnerSize.large}
-									label={
-										core.status.initialLoadingIndicatorText
-											? `Please wait: ${core.status.initialLoadingIndicatorText}`
-											: "Please wait"
-									}
-								/>
-							</div>
-						)}
-						{this.errorMessage ? (
-							<MessageBar
-								data-testid="error-msg"
-								messageBarType={MessageBarType.error}
-							>
-								{this.errorMessage}
-							</MessageBar>
-						) : (
-							""
-						)}
+						</PivotItem>
+					</Pivot>
+				) : (
+					<div className="spinner-container">
+						<Spinner
+							size={SpinnerSize.large}
+							label={
+								core.status.initialLoadingIndicatorText
+									? `Please wait: ${core.status.initialLoadingIndicatorText}`
+									: "Please wait"
+							}
+						/>
 					</div>
+				)}
+
+				{this.errorMessage ? (
+					<MessageBar
+						data-testid="error-msg"
+						messageBarType={MessageBarType.error}
+					>
+						{this.errorMessage}
+					</MessageBar>
+				) : (
+					""
 				)}
 				{(window as any).Cypress ? (
 					<DefaultButton
